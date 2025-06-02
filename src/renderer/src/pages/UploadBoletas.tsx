07b7f149f9b03cb3ebcee75e3126a630e2e4390a
Geer68 <germhidalgo@gmail.com>
2025-06-02 01:06:09 -0300

    fix: remove unused state variable in RecaudadoresPage and add eslint directive in UploadBoletas

--- src/renderer/src/pages/UploadBoletas.tsx
+++ src/renderer/src/pages/UploadBoletas.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState, useMemo } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
